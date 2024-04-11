d24a55284b67d01784a305181c44b3a8ccfda0b2
Arthur Sampaio <113317860+sampaio-arthh@users.noreply.github.com>
2024-04-11 15:07:03 -0300

    Correção e Atualização 11/04
    -> Troca de algoritmo
    --> Ao invés de um dicionário por atributo, os dicionários agora são por produto
    -->Novo processamento de código - utiliza uma função que retorna o código e com isso o compara com o recebido
    --> Programa exibe também as informações do produto antes de pedir as informações de compra(Produto e Quantidade)

--- quest2..py
+++ quest2..py
@@ -1,71 +1,34 @@
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+#Terminar de incluir: Função de compra(Verificar quantidade, apresentar preço)
+p1 = {"nome": "manteiga", "cod": 1289, "qtd": 413, "preco": 6.99}
+p2 = {"nome": "requeijao", "cod": 4189, "qtd": 30, "preco": 6.99}
+p3 = {"nome": "nescau", "cod": 1389, "qtd": 100, "preco": 6.99}
+p4 = {"nome": "queijo 300g", "cod": 76589, "qtd": 43, "preco": 14.98}
 
-p1 = Person("John", 36)
+def Cod(x):
+    return x["cod"];
 
-print(p1.name)
-print(p1.age)
+p = 0
 
-class Prod:
-    def __init__(self, code, nome, qtd, preco):
-        self.code = code
-        self.nome = nome
-        self.qtd = qtd
-        self.preco = preco
+lP = [p1, p2, p3, p4]
 
-p1 = Prod(2134,"manteiga", 50, 8.99)
-p2 = Prod(1289,"nescau", 16, 11.99)
-p3 = Prod(1241,"leite", 120, 4.59)
-p4 = Prod(42389,"requeijao", 18, 9.99)
-
-lProd = [p1, p2, p3, p4]
-
-cod = int(input("Código do produto]"))
+cod = int(input("Código do produto\n"))
 if cod == 0:
     print("Adeus")
 else:
-    for i in range(0, len(lProd)):
-        if cod not in ((lProd[i]).code)
-            print("Erro, código não listado")
-
-            while (cod not in prod_preco) and (cod!=0):
-
-                cod = int(input("Código de produto: "))
-                if cod not in prod_preco:
-                    print("Erro, código não listado")
-        
-        qtd = int(input(f"Quantidade do produto código = [{cod}]"))
-        if qtd <= prod_qtd[cod]:
-            print(f"Preço = {qtd*prod_preco[cod]:.2f}")
-            prod_qtd[cod] -= qtd
-        else:
-            print(f"Erro, quantidade solicitada maior que quantidade em estoque, só temos {prod_qtd[cod]}")
-
-
-prod_nm = {2134: "manteiga", 1289: "nescau", 1241: "leite", 42389: "requeijao"}
-prod_qtd = {2134: 50, 1289: 16, 1241: 120, 42389: 6}
-prod_preco = {2134: 10.89, 1289: 11.64, 1241: 4.80, 42389: 8.79}
-
-cod = 90909090909090
-
-while (cod!=0):
-    cod = int(input("Código de produto: "))
-    if cod != 0:
-
-        if cod not in prod_preco:
-            print("Erro, código não listado")
-
-            while (cod not in prod_preco) and (cod!=0):
-
-                cod = int(input("Código de produto: "))
-                if cod not in prod_preco:
-                    print("Erro, código não listado")
-        
-        qtd = int(input(f"Quantidade do produto código = [{cod}]"))
-        if qtd <= prod_qtd[cod]:
-            print(f"Preço = {qtd*prod_preco[cod]:.2f}")
-            prod_qtd[cod] -= qtd
-        else:
-            print(f"Erro, quantidade solicitada maior que quantidade em estoque, só temos {prod_qtd[cod]}")
+    cont=0
+    for i in lP:
+        if cod == Cod(i):
+            cont+=1
+            p = i
+            
+    if cont == 1:
+        print(f'Produto: {p["nome"]}, código: {p["cod"]}, quantidade em estoque: {p["qtd"]}, preço: {p["preco"]}')
+    else:
+        while cont!=1:
+            print("Erro, código indisponível")
+            cod = int(input("Código do produto\n"))
+            for i in lP:
+                if cod == Cod(i):
+                    p = i
+                    cont+=1
+        print(f'Produto: {p["nome"]}, código: {p["cod"]}, quantidade em estoque: {p["qtd"]}, preço: {p["preco"]}')
